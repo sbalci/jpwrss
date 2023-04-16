8b133a605cf48693c11519694917d24795d022b7
sbalci <drserdarbalci@gmail.com>
2023-04-16 23:02:05 +0300

    all functions are added as template

---
+++ R/z2corrs.b.R
@@ -0,0 +1,15 @@
+
+# This file is a generated template, your changes will not be overwritten
+
+z2corrsClass <- if (requireNamespace('jmvcore', quietly=TRUE)) R6::R6Class(
+    "z2corrsClass",
+    inherit = z2corrsBase,
+    private = list(
+        .run = function() {
+
+            # `self$data` contains the data
+            # `self$options` contains the options
+            # `self$results` contains the results object (to populate)
+
+        })
+)
---
+++ R/zpoisreg.b.R
@@ -0,0 +1,15 @@
+
+# This file is a generated template, your changes will not be overwritten
+
+zpoisregClass <- if (requireNamespace('jmvcore', quietly=TRUE)) R6::R6Class(
+    "zpoisregClass",
+    inherit = zpoisregBase,
+    private = list(
+        .run = function() {
+
+            # `self$data` contains the data
+            # `self$options` contains the options
+            # `self$results` contains the results object (to populate)
+
+        })
+)
